19d7b1d57ff121e2ae03b7c7bbc26ddacda6962f
alvintjw <E0958518@u.nus.edu>
2024-10-12 23:11:43 +0800

    Static Course Recommendation

--- next.config.js
+++ next.config.js
@@ -4,3 +4,21 @@
 module.exports = {
   reactStrictMode: false
 }
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/**'
+      }
+    ]
+  }
+}
